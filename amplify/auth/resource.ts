4fec9aa37db76b188f42f7db8c718732b8a07f91
Carlos Soto <carlosdedios0621@gmail.com>
2024-09-18 19:18:15 -0400

    viendo klk

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,33 @@
 import { defineAuth } from '@aws-amplify/backend';
-
+import { readFileSync } from 'fs';
+import path from 'path';
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+const xmlFilePath = path.join(process.cwd(), 'SAML', 'metadata.xml');
+const metadataContent = readFileSync(xmlFilePath, 'utf8');
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    externalProviders: {
+      saml: {
+        name: 'GoogleIDSAML',
+        metadata: {
+          metadataContent: metadataContent, // or content of the metadata file
+          metadataType: 'FILE', // or 'FILE'
+        },
+        attributeMapping: {
+          email: 'EMAIL',
+          familyName: 'Last name',
+          givenName: 'First name'
+        }
+      },
+      logoutUrls: ['http://localhost:3000/', 'https://main.d35s0i2t8frbrj.amplifyapp.com'],
+      callbackUrls: [
+        'http://localhost:3000/',
+        'https://main.d35s0i2t8frbrj.amplifyapp.com',
+      ],
+    },
   },
 });
